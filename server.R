f3f2edd1d42c481a6a617e47548ec68b59349956
unknown <amazroo@ncsu.edu>
2020-05-06 15:13:20 -0600

    + Add Documentation on NWMv2.0 and NWIS + Add Linear Model Regression summary

--- server.R
+++ server.R
@@ -7,8 +7,8 @@ library(raster)
 library(rgdal)
 library(moments)
 library(plotly)
-#library(smwrBase)
-#library(moments)
+library(reshape2)
+library(dygraphs)
 
 
 
@@ -27,7 +27,14 @@ basin_boundary_layer <- shapefile(paste0("AppDATA/basins.shp"))
 basin_layer.info <- read.table("AppDATA/info_stations.txt", sep=",", header=TRUE,
                                colClasses = c("siteID"="character"))
 
-list.units <- c("miu_obs" = "[cms]", "sigma_obs" = "[cms]", "miu_est" = "[cms]", "sigma_est" = "[cms]",
+list.metrics_out <- c("miu_obs" = "QObs-Mean", 
+                      "sigma_obs" = "Qobs-Standard Deviation",
+                      "miu_est" = "QNWMv2.0-Mean",
+                      "sigma_est" = "QNWM2.0-Standard Deviation",
+                      "MSE"="MSE", "KGE"="KGE", "NSE"="NSE", 
+                      "NSE_A"="NSE_A", "NSE_B"="NSE_B", "NSE_C"="NSE_C")
+
+list.units <- c("miu_obs" = "[m^3/s]", "sigma_obs" = "[m^3/s]", "miu_est" = "[m^3/s]", "sigma_est" = "[m^3/s]",
                   "MSE"="", "KGE"="", "NSE"="", "NSE_A"="", "NSE_B"="", "NSE_C"="")
 
 list.assessment_subnames <- list("Annual" = "overall", 
@@ -36,11 +43,13 @@ list.assessment_subnames <- list("Annual" = "overall",
 
 function(input, output, session) {
 
-
-  
-  
   # Filter the variable/data to display
- 
+  tmp.ind1 <- reactive({ basin_layer.info$CLASS %in% input$station_class })
+  tmp.var <- reactive({ as.numeric(metrics.all[tmp.ind1(),colnames(metrics.all) == paste0(input$metric,"_",input$assessment)]) })
+  colorpal <- reactive({ colorQuantile(input$colors, tmp.var(), n=10, na.color = "gray") })
+  data.tab3 <- reactive({ paste0("./AppDATA/Results_Assessment_NWM/STA_",input$tab3_siteID,".rds") %>% readRDS()})
+  data.flows <- reactive({ paste0("./Flows_1993_2018_V2/STA_",input$tab3_siteID,".rds") %>% readRDS()})
+  
   tmp.ind_RA <- reactive({ basin_layer.info$CLASS %in% input$RA_station_class & basin_layer.info$HUC2 %in% input$RA_huc})
   data.RA_x <- reactive({ as.numeric(basin_layer.info[tmp.ind_RA(), colnames(basin_layer.info) == input$RA_xvar]) })
   data.RA_y <- reactive({ as.numeric(metrics.all[tmp.ind_RA(),colnames(metrics.all) == paste0(input$RA_yvar,"_",input$RA_assessment)]) }) 
@@ -84,11 +93,80 @@ function(input, output, session) {
     clearGroup(group = "tmp.layer")
   })
 
+  
+  ## Interactive Map TAB ###########################################  
+  
+  observe({
+    # # Lables
+    labels <- sprintf("<strong>siteID#%s</strong><br/> %s <br/> %s <br/> %s = %.3f",
+                      basin_layer.info$siteID[tmp.ind1()],basin_layer$name[tmp.ind1()],
+                      basin_layer.info$CLASS[tmp.ind1()],
+                      paste0(list.metrics_out[input$metric]," (",input$assessment,")"),tmp.var()) %>% lapply(htmltools::HTML)
+    
+    pal <- colorpal()
+    if (!all(is.na(tmp.var()))) {   #prevent crash When unchecking all station classes
+      leafletProxy("map", data = basin_layer[tmp.ind1(),]) %>%
+        clearShapes() %>%
+        addCircles(lng = ~lon, lat = ~lat, weight = 1, label = labels, popup = labels.long[tmp.ind1()], 
+                   radius = 1+500*as.numeric(input$circlesize)^3,
+                   color = pal(tmp.var()), fillOpacity = 1, layerId = rownames(basin_layer.info[tmp.ind1(),]))
+    }
+  })
+  
+  observe({
+    proxy <- leafletProxy("map", data = basin_layer)
+    # Remove any existing legend, and only if the legend is
+    # enabled, create a new one.
+    proxy %>% clearControls()
+    pal <- colorpal()
+    pal_colors <- unique(pal(sort(tmp.var())))
+    pal_labs <- quantile(tmp.var(), seq(0, 1, .1), na.rm = TRUE) # depends on n from pal
+    pal_labs <- paste(sprintf("%.2f",lag(pal_labs)), sprintf("%.2f",pal_labs), sep = " - ")[-1] # first lag is NA
+    if (!all(is.na(tmp.var()))) {   #prevent crash When unchecking all station classes
+      proxy %>% addLegend(position = "bottomright", colors = pal_colors, labels = pal_labs,
+                          title = paste(list.metrics_out[input$metric],list.units[input$metric]), opacity = 1)
+      #proxy %>% addLegend(position = "bottomright", pal = pal, values = tmp.var(),
+      #                    labels = 1:10, title = paste(input$metric,"Percentile"))
+    }
+  })
+  
+  
   ## Regional Analyses (RA) TAB ###########################################
   observe({
     if (input$RA_xvar_log){ X <- log(data.RA_x()); X_label <- paste0("log(",input$RA_xvar,")")} else {X <- data.RA_x(); X_label <- input$RA_xvar}
     if (input$RA_yvar_log){ Y <- log(data.RA_y()); Y_label <- paste0("log(",input$RA_yvar,")")} else {Y <- data.RA_y(); Y_label <- input$RA_yvar}
-    tmp.dataly <- cbind(data.frame("x"=X, "y"=Y), basin_layer.info[tmp.ind_RA(),c("siteID","name","CLASS")])
+    tmp.dataly <- cbind(data.frame("x"=X, "y"=Y), basin_layer.info[tmp.ind_RA(),]) #c("DA","AI","dams","NID_str","Mx_strg","Nrml_st","siteID","name","CLASS")])
+    tmp.dataly <- do.call(data.frame,lapply(tmp.dataly, function(x) replace(x, is.infinite(x),NA)))
+    
+    #tmp.dataly <- cbind(data.frame("x"=log(basin_layer.info$DA), "y"=metrics.all$NSE_A_overall), basin_layer.info)
+    tmp.dataly <- tmp.dataly[rowSums(is.na(tmp.dataly[,unlist(lapply(tmp.dataly, is.numeric))])) == 0,]
+    rownames(tmp.dataly) <- NULL
+
+      if(length(input$lm_inputs>0)){
+        tmp.lm.expr <- paste("y ~ x+", paste(input$lm_inputs, collapse="+"))
+      } else {
+        tmp.lm.expr <- "y ~ x"
+      }
+
+      all_summaries <- list()
+      for (tmp.class in unique(tmp.dataly$CLASS)){
+        tmp.lm.data <- tmp.dataly[tmp.dataly$CLASS == tmp.class,]
+        tmp.lm <- lm(tmp.lm.expr, tmp.lm.data)
+        tmp.dataly[rownames(tmp.lm.data),"fitted_y"] <- tmp.lm$fitted.values
+        #print(paste(tmp.class,summary(tmp.lm), collapse = " "))
+        #output$lm_summary <- renderPrint({ summary(tmp.lm) })
+        tmp.summary <- summary(tmp.lm)
+        tmp.lm.expr2 <- tmp.lm.expr
+        tmp.lm.expr2 <- sub("x",X_label,tmp.lm.expr2)
+        tmp.lm.expr2 <- sub("y",Y_label,tmp.lm.expr2)
+        tmp.summary$call <- paste(tmp.lm.expr2, ", n=",dim(tmp.lm.data)[1])
+        rownames(tmp.summary$coefficients) <- sub("x",X_label,rownames(tmp.summary$coefficients))
+        all_summaries[[tmp.class]] <- tmp.summary
+      }
+      names(all_summaries) <- paste(toupper(names(all_summaries)),"FLOWS")
+      output$lm_summary <- renderPrint({ all_summaries })
+
+    tmp.dataly <- tmp.dataly[order(tmp.dataly$x),]
     output$RA_plot <- renderPlotly({
       plot_ly(type = 'scatter',  mode='markers', tmp.dataly,
               x = ~x, y = ~y, color=~CLASS, colors = c("#17becf","#ff7f0e"),
@@ -98,15 +176,86 @@ function(input, output, session) {
                 "%{yaxis.title.text}: %{y:.2f}<br>",
                 "%{xaxis.title.text}: %{x:.2f}",
                 "<extra></extra>"
-              ))%>%
-       layout(title = input$RA_assessment,
+              ), name = "data_points")%>%
+        add_trace(type = "scatter",mode = "lines", name = "fitted_y",
+                  x = ~x, y = ~fitted_y, color=~CLASS, colors = c("#17becf","#ff7f0e"),
+                  text = sprintf("%s\n%s\nBasin Type: %s", paste("siteID=",tmp.dataly$siteID), tmp.dataly$name, tmp.dataly$CLASS),
+                  hovertemplate = paste(
+                    "<b>%{text}</b><br>",
+                    "%{yaxis.title.text}_fitted: %{y:.2f}<br>",
+                    "%{xaxis.title.text}: %{x:.2f}",
+                    "<extra></extra>"
+                  )) %>%
+        layout(title = input$RA_assessment,
               xaxis = list(title = X_label),
               yaxis = list(title = Y_label),
+              showlegend = FALSE,
               legend = list(orientation = 'h')) %>%
         config( displayModeBar = "hover", displaylogo = FALSE,
                 modeBarButtonsToRemove = c("select2d","lasso2d", "toggleSpikelines","autoScale2d",
                                            "hoverClosestCartesian", "hoverCompareCartesian"))
     })
+    
+
+    
+    output$download_RA_data <- downloadHandler(
+      filename = "data.csv",
+      content = function(file) {
+        readr::write_csv(tmp.dataly, file)
+    })
+    
+    
   })
+  
+  ## SINGLE BASIN TAB ###########################################
+  ## Tab3 - BASIC INFO
+  observe({
+    data.tab3_info1 <- basin_layer.info[basin_layer.info$siteID==input$tab3_siteID,]
+    data.tab3_info1 <- data.tab3_info1[,!(colnames(data.tab3_info1) %in% c("obs","FID"))]
+    output$tab3_info1 <- renderTable(data.tab3_info1[,1:10])
+    output$tab3_info11 <- renderTable(data.tab3_info1[,11:17])
+    
+    data.tab3_info2 <- metrics.overall[metrics.overall$siteID==input$tab3_siteID,-1]
+    colnames(data.tab3_info2) <- paste(list.metrics_out,list.units)
+    output$tab3_info2 <- renderTable(data.tab3_info2)
+  })
+  
+  #### Tab3 - Time Series PLOT
+  observe({
+    
+    data.tab3_plot <- subset(data.tab3() , names(data.tab3()) %in% list.assessment_subnames[[input$tab3_plot_assessment]]) %>%
+      sapply('[[',input$tab3_plot_metric)
+    
+    flows <- data.flows()
+    flows[flows$Qnwm<0 , "Qnwm"] <- 0
+    flows_ts <- xts::xts(x=flows[,c("Qobs","Qnwm")], order.by = flows$Date)
+
+    output$tab3_dygraph_flows <- renderDygraph({
+      dygraph(flows_ts, ylab = "Streamflow [m^3/s]", main = "NWMv2.0 predictions vs. USGS observations") %>%
+        dyRangeSelector()
+    })
+
+    #### Tab3 - BOX PLOT    
+    output$tab3_plot <- renderPlotly({
+      plot_ly(type='box', melt(as.data.frame(data.tab3_plot), id.vars = NULL),
+              x = ~variable, y = ~value, col="lightblue") %>%
+        layout(title = "26 years of data",
+               xaxis = list(title = ""),
+               yaxis = list(title = paste(list.metrics_out[input$tab3_plot_metric],list.units[input$tab3_plot_metric])),
+               height = 500,
+               width = 100*ncol(data.tab3_plot)^.8 + 150) %>%
+            config( displayModeBar = "hover", displaylogo = FALSE,
+                    modeBarButtonsToRemove = c("select2d","lasso2d", "toggleSpikelines","autoScale2d",
+                                               "hoverClosestCartesian", "hoverCompareCartesian"))
+    })
+
+    data.tab3_detaileddata <- cbind("year"=as.character(1993:2018), round(data.tab3_plot, digits = 2))
+    colnames(data.tab3_detaileddata)[colnames(data.tab3_detaileddata)=="overall"] <- paste(list.metrics_out[input$tab3_plot_metric],list.units[input$tab3_plot_metric])
+    output$tab3_table <- renderTable(data.tab3_detaileddata, 
+                                     caption = paste(list.metrics_out[input$tab3_plot_metric],list.units[input$tab3_plot_metric]),
+                                     caption.placement = getOption("xtable.caption.placement", "top")
+                                     )
+  })
+  
       
 }
--- ui.R
+++ ui.R
@@ -2,6 +2,7 @@ library(shiny)
 library(leaflet)
 library(RColorBrewer)
 library(plotly)
+library(dygraphs)
 
 list.assessments <- c("Overall (1993-2018)"="overall",
                       "-Winter"="winter","-Spring"="spring",
@@ -12,8 +13,8 @@ list.assessments <- c("Overall (1993-2018)"="overall",
                       "--Oct"="Oct","--Nov"="Nov","--Dec"="Dec")
 list.metrics <- c("QObs-Mean" = "miu_obs", 
                   "Qobs-Standard Deviation" = "sigma_obs",
-                  "QNWM-Mean" = "miu_est",
-                  "QNWM-Standard Deviation" = "sigma_est",
+                  "QNWMv2.0-Mean" = "miu_est",
+                  "QNWMv2.0-Standard Deviation" = "sigma_est",
                   "MSE"="MSE", "KGE"="KGE", "NSE"="NSE", 
                   "NSE_A"="NSE_A", "NSE_B"="NSE_B", "NSE_C"="NSE_C")
 basin_layer.info <- read.table("AppDATA/info_stations.txt", sep=",", header=TRUE,
@@ -28,7 +29,7 @@ names(list.hucs) <- paste0("huc",sprintf("%02d",1:18))
 label.siteID_names <- basin_layer.info$siteID
 names(label.siteID_names) <- paste(basin_layer.info$siteID, basin_layer.info$name)
 
-navbarPage("Online NWM Performance Analysis Tool", id="nav",
+navbarPage("Online NWMv2.0 Performance Analysis Tool", id="nav",
 
 
 ################# THE MAP #################
@@ -38,13 +39,75 @@ navbarPage("Online NWM Performance Analysis Tool", id="nav",
                 includeCSS("styles.css")
             ),
             
-            leafletOutput("map", width="100%", height="100%")
+            leafletOutput("map", width="100%", height="100%"),
+            absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                          draggable = FALSE, top = 65, right = 0 , width = 300, #bottom = 10,
+                          #width = "25%", height = "90%",
+                          
+                          #h5("1) Select a station from the map"),
+                          selectInput("assessment", h5("Performance Assessment"), choices = list.assessments,
+                                      selected = list.assessments[1]),
+                          selectInput("metric", h5("Verification Metric"), choices=list.metrics,
+                                      selected = list.metrics[1]),
+                          checkboxGroupInput("station_class", "", c("Natural flows (583)" = "natural","Controlled flows (2884)" = "non-natural"),
+                                             selected = c("natural","non-natural"))
+                          ),
+            
+            absolutePanel(id = "legend-controls", class = "panel panel-default", fixed = FALSE,
+                          draggable = TRUE, top = 90, left = 11 , width = 180, height = 170,
+                          selectInput("colors", "Color Scheme",
+                                      rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
+                                      , selected = "RdYlBu"),
+                          numericInput("circlesize", "Marker Size", value = 2, min = 0, max = 5, step = 0.1)
+                          )
             ),
 
             tags$div(id="cite",
-                     tags$em(' Online NWM Performance Analysis Tool; 2020 ')
+                     tags$em(' Online NWMv2.0 Performance Analysis Tool; 2020 ')
             )
    ),
+
+
+
+################# SINGLE STATION TAB #################
+tabPanel("Single Site Analyses",
+         fluidRow(
+            column(10,
+                   selectizeInput("tab3_siteID",label = h4("select a station"),
+                                  choices=label.siteID_names, options= list(maxOptions = 4000), width = 580, )
+            )
+         ),
+      
+         h3("Basic Info:"),
+         tableOutput("tab3_info1"),
+         tableOutput("tab3_info11"),
+         dygraphOutput("tab3_dygraph_flows"),
+         br(),
+         h4("Overall Performance of NWMv2.0 flows (1993-2018)"),
+         tableOutput("tab3_info2"),
+         headerPanel(""),
+         
+         fluidRow(
+         column(3, offset = 0, style='padding-top:0px; padding-bottom:0px;',
+                selectInput("tab3_plot_assessment", h5("Assessment Type"), choices = c("Annual","Seasonal","Monthly"),
+                            selected = "Annual")),
+         column(3,
+                selectInput("tab3_plot_metric", h5("Verification Metric"), choices=list.metrics,
+                            selected = list.metrics[1]))
+         ),
+
+         tabsetPanel(
+            tabPanel("Boxplot",
+                     br(), br(),
+                     plotlyOutput("tab3_plot"),
+                     br()),
+            
+            tabPanel("Detailed Data",
+                     br(),
+                     tableOutput("tab3_table"),
+                     br())
+         )
+),
 
 
 
@@ -53,21 +116,31 @@ tabPanel("Regional Analyses",
          sidebarLayout(
             sidebarPanel(selectizeInput("RA_huc", h5("HUC Region(s)"), choices = list.hucs, selected = list.hucs[1],
                                         multiple = TRUE, options = list('plugins' = list('remove_button'),
-                                           'create' = TRUE, 'persist' = FALSE)),
-                         checkboxGroupInput("RA_station_class", "", c("Natural Basins" = "natural","Controlled Basins" = "non-natural"),
+                                                                        'create' = TRUE, 'persist' = FALSE)),
+                         checkboxGroupInput("RA_station_class", "", c("Natural flows" = "natural","Controlled flows" = "non-natural"),
                                             selected = c("natural","non-natural")),
                          selectInput("RA_assessment", h5("Assessment"), choices = list.assessments,
                                      selected = list.assessments[1]),
-                         selectInput("RA_xvar", h5("x variable"), choices = list.basin_characters,
+                         selectInput("RA_xvar", h5("x variable"), choices = list.basin_characters[-(1:2)],
                                      selected = "DA"),
                          checkboxInput("RA_xvar_log", h5("x in log space"), value = TRUE),
                          selectInput("RA_yvar", h5("y variable"), choices = list.metrics,
                                      selected = "NSE_A"),
-                         checkboxInput("RA_yvar_log", h5("y in log space"), value = FALSE)
+                         checkboxInput("RA_yvar_log", h5("y in log space"), value = FALSE),
+                         hr(), tags$hr(style="border-color: black;"),
+                         h5('Fit a Linear Regression Model to "y variable"'),
+                         selectizeInput("lm_inputs", h5("additional predictors (besides x variable)"), choices = list.basin_characters[-(1:2)], selected = NULL,
+                                        multiple = TRUE, options = list('plugins' = list('remove_button'),
+                                                                        'create' = TRUE, 'persist' = FALSE))
+                         
             ),
             mainPanel(
                br(),
-               plotlyOutput("RA_plot", height="600px")
+               plotlyOutput("RA_plot", height="500px"),
+               #downloadButton("download_RA_data", label = "Download Data")
+               br(),
+               h3("Summary of Linear Regression Model:"),
+               verbatimTextOutput("lm_summary")
             )
          )
 ),
@@ -76,8 +149,14 @@ tabPanel("Regional Analyses",
 ################# DOCUMENTATION TAB #################
    tabPanel("Documentation",
             tags$div(
-               tags$h4("User guide / methodology"),
                tags$br(),
+               tags$em(tags$h1("Data Description")),
+               tags$h4("Observed streamflow data is obtained from USGS that maintains and operates a network of observation stations that regularly monitor and report streamflow conditions to the National Water Information System (NWIS). In total there are ~7000 active gages. While an impressive observation network, these gages only monitor ~0.25% of the 2,700,000 river segments digitized in the National Hydrography Dataset (NHDPlusV2)."),
+               #tags$br(),
+               tags$h4("Streamflow predictions are obtained form the National Water Model version 2.0 (NWMv2.0) which succeeded versions 1.0 (year 2016) and 1.2 (year 2017). With each new version, NCAR and NOAA produce a multi-decade reanalysis product with NLDAS  {Xia:2019tt} and NARR meteorological products to provide a historic hourly simulation of channel conditions and 3-hour simulations of land surface conditions. The raw output from these simulations are made available to the public on a 'best effort' basis in an Amazon S3 bucket. These reanalysis products are calibrated in a limited number of basins and do not assimilate NWIS observations in the same way that the operational models do. That said, the retrospective rainfall products provided by NLDAS decrease some of the uncertainty that would be evident in the operational product. ",tags$a(href="https://water.noaa.gov/about/nwm","https://water.noaa.gov/about/nwm")),
+               #tags$br(),
+               tags$h4("Basin boundaries were delinatied using the Network Linked Data Index (NLDI) to identify the coloacted NWIS and NHD identifiers and then the trace the upstream tributary along the NHDPlusV2 networks. The individual NHD catchments along the upstream flowpath were then unioned into a single basin boundary."),
+               tags$hr(),
                tags$img(src="Assessment.png", width=1000),
                tags$br(),
                tags$img(src="MSE.png", width=1000),
